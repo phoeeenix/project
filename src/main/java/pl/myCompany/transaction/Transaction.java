1cffd90f1b5d141957c53c968bdf7048adbf0801
pawel.beres <win_dows@o2.pl>
2020-05-17 12:33:45 +0200

    Transaction initial #3

---
+++ src/main/java/pl/myCompany/transaction/Transaction.java
@@ -0,0 +1,43 @@
+package pl.myCompany.transaction;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Getter
+@Setter
+public class Transaction {
+
+  public Transaction() {
+  }
+
+  public Transaction(Long id, String description, Long categoryId, LocalDate localDate, boolean isPlanned) {
+    this.id = id;
+    this.description = description;
+    this.categoryId = categoryId;
+    this.localDate = localDate;
+    this.isPlanned = isPlanned;
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String description;
+
+  private Long categoryId;
+
+  private LocalDate localDate;
+
+  //private List<AccountPriceEntry> accountPriceEntryList;
+
+  private boolean isPlanned;
+
+}
